5937fb9273eb227eec05e45cfccbb9b606728b72
John Zhao <john@zondr.com>
2015-06-22 11:23:50 +0000

    validate data

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -12,8 +12,13 @@ class PlacesController < ApplicationController
 	end
 
 	def create
-		current_user.places.create(place_params)
-		redirect_to root_path
+		@place = current_user.places.create(place_params)
+			if @place.valid?
+				redirect_to root_path
+			else
+				render :new, :status => :unprocessable_entity
+			end
+
 	end
 
 	def show
@@ -33,9 +38,12 @@ class PlacesController < ApplicationController
 			return render :text => 'Not Allowed', :status => :forbidden
 		end
 
-
 		@place.update_attributes(place_params)
-		redirect_to root_path
+		if @place.valid?
+			redirect_to root_path
+		else
+			render :edit, :status => :unprocessable_entity
+		end
 	end	
 
 	def destroy
@@ -43,7 +51,7 @@ class PlacesController < ApplicationController
 		if @place.user != current_user
 			return render :text => 'Not Allowed', :status => :forbidden
 		end
-		
+
 		@place.destroy
 		redirect_to root_path
 	end
